6ca9c5df931cf9f09c877b5fe09d90a8228c0b97
Munib Hasnain <munibh15@gmail.com>
2020-05-14 23:22:30 -0400

    gpfa

--- matToCsv.m
+++ matToCsv.m
@@ -9,6 +9,30 @@ data = forGPFA.dat;
 
 numTrials = size(data,2);
 
+% for i=1:numTrials
+%     csvwrite(strcat('data/trial',int2str(i),'.csv'),data(i).spikes);
+% end
+
+RT = [];
+cue = [];
+choice = [];
 for i=1:numTrials
-    csvwrite(strcat('data/trial',int2str(i),'.csv'),data(i).spikes);
+    RT(i) = data(i).RT;
+    cue(i) = data(i).Cue;
+    choice(i) = data(i).choice;
 end
+
+RT = RT';
+cue = cue';
+choice = choice';
+
+exData = [RT, cue, choice];
+
+csvwrite('data/RT_cue_choice.csv',exData);
+
+
+
+
+
+
+
